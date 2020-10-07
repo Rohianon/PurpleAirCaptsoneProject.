44742c076d1d13ddffbdcdad53a77d7fe524e5e1
Rohianon <rohianon48@gmail.com>
2020-10-07 15:52:47 +0300

    UPdated

--- Scripts/purpleair.py
+++ Scripts/purpleair.py
@@ -2,3 +2,25 @@ import pandas as pd
 import dask.dataframe as dd
 
 df = dd.read_csv('Data/20*.csv')
+
+
+import functools
+
+#decorators use a function but modifies it so that the function becomes something else
+
+def my_decor(func):
+    @functools.wraps(func)
+    def function_runs_func():
+        print('In the Decorator')
+        func()
+        print('After the decorator')
+    #this line is v.imp. Here the modified function is returned.
+    #so we pass in another function defined below and adds extra thing to it
+    return function_runs_func()
+
+#this function gets replaced by function_runs_func()
+@my_decor
+def my_func():
+    print('I am the funciont!')
+
+my_func()
